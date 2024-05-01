944ed365a1b0b74317838614a1481db50102d372
holmes <wuyueholmes@outlook.com>
2024-05-01 15:52:43 -0400

    new version that fixes Azure OpenAI API

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import pkg_resources
 import pathlib
 
 PKG_NAME = "agentkit-llm"
-VERSION = "0.1.6.post1"
+VERSION = "0.1.6.post2"
 EXTRAS = {
     "logging": ["wandb"],
     "all": ["wandb", "openai", "anthropic", "tiktoken"],
